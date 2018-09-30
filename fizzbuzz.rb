4c0f6492c022729642d817c2d4b88e52d56c6238
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-30 00:29:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 5== 0 && int% 3 ==0
+    puts "FizzBuzz"
+  end
 end
